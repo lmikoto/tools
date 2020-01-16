c26d9a4c0598622131d7d28784e83facf6ee6437
liuyang <ly193933@alibaba-inc.com>
2020-01-16 10:27:20 +0800

    feat: json parse 初步

--- src/pages/json/index.tsx
+++ src/pages/json/index.tsx
@@ -1,11 +1,48 @@
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Input, Row, Col } from 'antd';
+import JSONTree from 'react-json-tree';
 import styles from './index.less';
+import theme from './tree-theme';
+
+const { TextArea } = Input;
 
 export default () => {
+  const [value, setValue] = useState('');
+
+  const getJson = (str: string) => {
+    try {
+      return JSON.parse(str);
+    } catch (e) {
+      return {};
+    }
+  };
+
   return (
     <PageHeaderWrapper title={false} className={styles.main}>
-      <div style={{ paddingTop: 100, textAlign: 'center' }}></div>
+      <div className={styles.container}>
+        <Row>
+          <Col span={10}>
+            <TextArea
+              value={value}
+              onChange={e => setValue(e.target.value)}
+              className={styles.textarea}
+            />
+          </Col>
+          <Col span={14}>
+            <div className={styles.treeContainer}>
+              <JSONTree
+                data={getJson(value)}
+                shouldExpandNode={() => true}
+                getItemString={() => null}
+                hideRoot
+                theme={theme}
+              />
+            </div>
+          </Col>
+        </Row>
+      </div>
     </PageHeaderWrapper>
   );
 };
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -38,3 +38,4 @@ declare let ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: 'site' | undefine
 declare let BACKEND_URL: string;
 
 declare module 'cron-converter';
+declare module 'react-json-tree';
